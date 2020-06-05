a14f4a6c8f1e620e25c5849580e6b0ddbe881983
Andy Yglecias <andyyglecias@gmail.com>
2020-06-04 20:53:43 -0500

    Added data for serum app

---
+++ src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <div>
+    <h4>{{ info }}</h4>
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  data: function() {
+    return {
+      info: "Plant-based formulated products like mine contains vitamins, minerals, and antioxidents which will help repair and keep the skin and hair hydrated and nourished everyday. Therefore, feeding your skin and hair with all-natural, vegan ingrediants will continue for flourish it.",
+      
+      importance: "It's so important to pay attnetion to the products you're using on your skin on average absorbs 64% of the products we apply to it. So if those products that you are using contain harsh chemicals and  toxins, are synthetically processed and filled with preservatives, it will cause unhealthy damage to the skin.",
+      
+      breakdown: "The same logic applies to your diet, if you don't drink water, or don't eat enough food that contains the vitamins and nutrients that your body needs, you will eventually be unhealthy. An important thing to remember is that nutritional status plays an essential role in the continuance of healthy, glowing skin.",
+      
+      sellpoint: "You will feel and will be much healthier if you stick to eating nutritious food. So if you chang up your diet for important health reasons, why won't you switch to clean beauty products for similar reasons?",
+
+      faceserum: "My all-natural, organic Facial Serum is an anti-aging and anti-acne face treatment designed to hydrate and replenish the skin. Nutrients in the serum support cell regeneration and prevent cell damage. It is rich in antioxidants and anti-inflammatory properties that help deeply moisturize, reduces and prevents redness, acne scars, fine lines and wrinkles.",
+
+      fsingredients: "Organic Roseship Seed Oil, Organin Hemp Seed Oil, Organic Pomengranate Seed Oil, Organic Grape Seed Oil, Organic Moroccan Argan Oil",
+
+      hserum: "My all-natural, organic Hair Serum is a daily hair treatment designed to vitalize and adds lots of shine yo your hair, leaving it looking beautifully conditioned! It is packed with pure, hydrating ingredients that promote healthy hair by strengthening and moisturizing. It helps prevent and control frizziness, humidity, and adds shine without making your hair look greasy! It also promotes hair growth and eliminates dandruff when massaged to the scalp twice a week. Essential oils blended in the serum are known to refresh, energize, and uplift your mood!",
+
+      hsingredients: "Organin Jojoba Oil, Organic Grape Seed Oil, Organic Moroccan Argan Oil, Bergamot, Grapefruit, Orange, Lime, and Lemon Organin essential Oils."
+      
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
+</script>
+
