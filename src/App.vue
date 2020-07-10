639b95fd3bad82dfaf7b4c0bf6d00cc2b76df40e
Andy Yglecias <andyyglecias@gmail.com>
2020-07-09 22:25:17 -0500

    changed background color

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,146 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    
+    <nav class="navbar navbar-dark navbar-fixed-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header page-scroll">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                        <i class="fa fa-bars"></i>
+                    </button>
+                    <a class="navbar-brand" href="#page-top">
+                        <span class="logo">
+                            <img src="assets/img/logo.png" alt="">
+                        </span>
+                    </a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                    <ul class="nav navbar-nav">
+                        <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                        <li class="hidden">
+                            <a href="#page-top"></a>
+                        </li>
+                        <li class="page-scroll">
+                            <a href="#about">About</a>
+                        </li>
+                        <li class="page-scroll">
+                            <a href="#features">Features</a>
+                        </li>
+                        <li class="page-scroll">
+                            <a href="#work">Work</a>
+                        </li>
+                        <li class="page-scroll">
+                            <a href="#blog">Blog</a>
+                        </li>
+                        <li class="page-scroll">
+                            <a href="#pricing">Pricing</a>
+                        </li>
+                        <li class="page-scroll">
+                            <a href="#contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container -->
+    </nav>
+
+
+     <aside class="bg-parallax-dark nopadding" style="background-image: url(https://images.pexels.com/photos/1477166/pexels-photo-1477166.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940);" data-stellar-background-ratio=".5">
+            <div class="aside-overlay">
+                <div class="container">
+                    <div class="row text-center">
+                        <div class="col-lg-8 col-lg-offset-2">
+                            <h2>Glowing Garden</h2>
+                            <hr class="light">
+                            <h4>The healthy way to GLOW</h4>
+                               </div>
+                    </div>
+                </div>
+            </div>
+        </aside>
+
+
+        
+  <router-view/>
+
+  <footer class="footer-1">
+            <div class="upper">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <h2 class="script">Glowing Garden</h2>
+                            <address>
+                                <strong>Twitter, Inc.</strong>
+                                <br>795 Folsom Ave, Suite 600
+                                <br>San Francisco, CA 94107
+                                <br>
+                                <abbr title="Phone">P:</abbr> (123) 456-7890
+                            </address>
+                        </div>
+                        <div class="col-md-3">
+                            <h4>Features</h4>
+                            <ul class="list-unstyled footer-links">
+                                <li>
+                                    <a href="#">Responsive</a>
+                                </li>
+                                <li>
+                                    <a href="#">Easy to Use</a>
+                                </li>
+                                <li>
+                                    <a href="#">Well Documented</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-md-3">
+                            <h4>Documentation</h4>
+                            <ul class="list-unstyled footer-links">
+                                <li>
+                                    <a href="#">Bootstrap Components</a>
+                                </li>
+                                <li>
+                                    <a href="#">Usage Information</a>
+                                </li>
+                                <li>
+                                    <a href="#">Full Documentation</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-md-3">
+                            <h4>Social</h4>
+                            <ul class="list-inline">
+                                <li>
+                                    <a href="#" class="btn btn-social-light btn-facebook"><i class="fa fa-fw fa-facebook"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#" class="btn btn-social-light btn-twitter"><i class="fa fa-fw fa-twitter"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#" class="btn btn-social-light btn-dribbble"><i class="fa fa-fw fa-dribbble"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#" class="btn btn-social-light btn-google-plus"><i class="fa fa-fw fa-google-plus"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="lower">
+                <div class="container">
+                    <div class="row">
+
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+
+
+
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,252 @@
 <template>
   <div class="home">
     <div>
-    <h4>{{ info }}</h4>
+      <section id="about" class="text-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <h2>About</h2>
+                        <hr class="primary">
+                        <p>
+                          {{ info }}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
+
+    <section id="pricing" class="pricing-1">
+            <div class="container">
+                <div class="row text-center">
+                    <div class="col-md-8 col-md-offset-2">
+                        <h2>Products</h2>
+                        <hr class="primary">
+                        <p>Three licensing options are availalbe for the Spectrum Template. A single application license, a multiple application license, and an extended license are the three options availalbe.</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4 pricing-col" data-scrollreveal="enter left over 1.5s">
+                        <div class="pricing-table">
+                            <div class="pricing-heading" style="background-image: url('assets/img/demo-pricing/1.jpg')">
+                                <h2>Vitalize and Shine Hair Serum</h2>
+                            </div>
+                            <div class="content">
+                                <div class="price">
+                                    <span class="amount">$20</span>
+                                    <span class="period">one time</span>
+                                </div>
+                                <ul class="pricing-items list-unstyled text-center">
+                                    <li class="item">
+                                        <span class="text-primary">Ingredients {{ hsingredients }}</span> 
+                                    <li class="pricing-button">
+                                        <a href="#" class="btn btn-primary btn-square btn-raised">Buy Now</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 pricing-col" data-scrollreveal="enter bottom over 1.5s">
+                        <div class="pricing-table featured">
+                            <div class="pricing-heading" style="background-image: url('assets/img/demo-pricing/2.jpg')">
+                                <h2>Bundle Deal!</h2>
+                            </div>
+                            <div class="content">
+                                <div class="price">
+                                    <span class="amount">$40</span>
+                                    <span class="period">Save $8!</span>
+                                </div>
+                                <ul class="pricing-items list-unstyled text-center">
+                                    <li class="item">Limited Time offer!</li>
+                                    <li class="pricing-button">
+                                        <a href="#" class="btn btn-primary btn-square btn-raised">Buy Now</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 pricing-col" data-scrollreveal="enter right over 1.5s">
+                        <div class="pricing-table">
+                            <div class="pricing-heading" style="background-image: url('assets/img/demo-pricing/1.jpg')">
+                                <h2>Hydrate and Replenish Facial Serum</h2>
+                            </div>
+                            <div class="content">
+                                <div class="price">
+                                    <span class="amount">$28</span>
+                                    <span class="period">one time</span>
+                                </div>
+                                <ul class="pricing-items list-unstyled text-center">
+                                    <li class="item">
+                                        <span class="text-primary">Ingredients: {{ fsingredients }}</span>
+                                    <li class="pricing-button">
+                                        <a href="#" class="btn btn-primary btn-square btn-raised">Buy Now</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+
+        <section id="work" class="portfolio-1">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <h2>Testimonials</h2>
+                        <hr class="primary">
+                        <ul class="list-inline hidden-xs" id="filters">
+                            <li>
+                                <button data-filter="*" type="button" class="btn btn-primary btn-square btn-raised">All</button>
+                            </li>
+                            <li>
+                                <button data-filter=".web" type="button" class="btn btn-primary btn-square btn-raised">Website</button>
+                            </li>
+                            <li>
+                                <button data-filter=".graphic" type="button" class="btn btn-primary btn-square btn-raised">Graphic</button>
+                            </li>
+                            <li>
+                                <button data-filter=".print" type="button" class="btn btn-primary btn-square btn-raised">Print</button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row" data-scrollreveal="enter bottom over 1.5s">
+                    <div class="isotope">
+                        <div class="col-sm-4 portfolio-item web">
+                            <a href="#portfolio-modal-1" class="portfolio-link" data-toggle="modal">
+                                <div class="caption">
+                                    <div class="caption-content">
+                                        <h3><i class="fa fa-search fa-2x"></i>
+                                        </h3>
+                                    </div>
+                                </div>
+                                <img src="assets/img/demo-portfolio/1.jpg" class="img-centered" alt="">
+                            </a>
+                        </div>
+                        <div class="col-sm-4 portfolio-item graphic">
+                            <a href="#portfolio-modal-2" class="portfolio-link" data-toggle="modal">
+                                <div class="caption">
+                                    <div class="caption-content">
+                                        <h3><i class="fa fa-search fa-2x"></i>
+                                        </h3>
+                                    </div>
+                                </div>
+                                <img src="assets/img/demo-portfolio/2.jpg" class="img-centered" alt="">
+                            </a>
+                        </div>
+                        <div class="col-sm-4 portfolio-item print">
+                            <a href="#portfolio-modal-1" class="portfolio-link" data-toggle="modal">
+                                <div class="caption">
+                                    <div class="caption-content">
+                                        <h3><i class="fa fa-search fa-2x"></i>
+                                        </h3>
+                                    </div>
+                                </div>
+                                <img src="assets/img/demo-portfolio/3.jpg" class="img-centered" alt="">
+                            </a>
+                        </div>
+                        <div class="col-sm-4 portfolio-item web">
+                            <a href="#portfolio-modal-2" class="portfolio-link" data-toggle="modal">
+                                <div class="caption">
+                                    <div class="caption-content">
+                                        <h3><i class="fa fa-search fa-2x"></i>
+                                        </h3>
+                                    </div>
+                                </div>
+                                <img src="assets/img/demo-portfolio/4.jpg" class="img-centered" alt="">
+                            </a>
+                        </div>
+                        <div class="col-sm-4 portfolio-item graphic">
+                            <a href="#portfolio-modal-1" class="portfolio-link" data-toggle="modal">
+                                <div class="caption">
+                                    <div class="caption-content">
+                                        <h3><i class="fa fa-search fa-2x"></i>
+                                        </h3>
+                                    </div>
+                                </div>
+                                <img src="assets/img/demo-portfolio/5.jpg" class="img-centered" alt="">
+                            </a>
+                        </div>
+                        <div class="col-sm-4 portfolio-item print">
+                            <a href="#portfolio-modal-2" class="portfolio-link" data-toggle="modal">
+                                <div class="caption">
+                                    <div class="caption-content">
+                                        <h3><i class="fa fa-search fa-2x"></i>
+                                        </h3>
+                                    </div>
+                                </div>
+                                <img src="assets/img/demo-portfolio/6.jpg" class="img-centered" alt="">
+                            </a>
+                        </div>
+                        <div class="col-sm-4 portfolio-item web">
+                            <a href="#portfolio-modal-2" class="portfolio-link" data-toggle="modal">
+                                <div class="caption">
+                                    <div class="caption-content">
+                                        <h3><i class="fa fa-search fa-2x"></i>
+                                        </h3>
+                                    </div>
+                                </div>
+                                <img src="assets/img/demo-portfolio/7.jpg" class="img-centered" alt="">
+                            </a>
+                        </div>
+                        <div class="col-sm-4 portfolio-item graphic">
+                            <a href="#portfolio-modal-1" class="portfolio-link" data-toggle="modal">
+                                <div class="caption">
+                                    <div class="caption-content">
+                                        <h3><i class="fa fa-search fa-2x"></i>
+                                        </h3>
+                                    </div>
+                                </div>
+                                <img src="assets/img/demo-portfolio/8.jpg" class="img-centered" alt="">
+                            </a>
+                        </div>
+                        <div class="col-sm-4 portfolio-item print">
+                            <a href="#portfolio-modal-2" class="portfolio-link" data-toggle="modal">
+                                <div class="caption">
+                                    <div class="caption-content">
+                                        <h3><i class="fa fa-search fa-2x"></i>
+                                        </h3>
+                                    </div>
+                                </div>
+                                <img src="assets/img/demo-portfolio/9.jpg" class="img-centered" alt="">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+     
   </div>
 </template>
 
 <style>
+
+
+
+
 </style>
 
 <script>
 export default {
   data: function() {
     return {
-      info: "Plant-based formulated products like mine contains vitamins, minerals, and antioxidents which will help repair and keep the skin and hair hydrated and nourished everyday. Therefore, feeding your skin and hair with all-natural, vegan ingrediants will continue for flourish it.",
+      info: "At Glowing Garden, our primary focus is making sure our products consists of healthy and all-natural ingrediants. Our serums are made up of Plant-based formulated products that contain vitamins, minerals, and antioxidents which will help repair and and replenish your skin and hair, leading to much more hydrated and nourished results.",
       
-      importance: "It's so important to pay attnetion to the products you're using on your skin on average absorbs 64% of the products we apply to it. So if those products that you are using contain harsh chemicals and  toxins, are synthetically processed and filled with preservatives, it will cause unhealthy damage to the skin.",
+      importance: "It's critical that you pay attnetion to the products you're using, especially when it comes to your skin. On average, your skin will absorb around 64% of the products we apply to it. So if those products you're currently using contain harsh chemicals and toxins, are synthetically processed and filled with preservatives, it will lead to unhealthy  and damaged to the skin.",
       
       breakdown: "The same logic applies to your diet, if you don't drink water, or don't eat enough food that contains the vitamins and nutrients that your body needs, you will eventually be unhealthy. An important thing to remember is that nutritional status plays an essential role in the continuance of healthy, glowing skin.",
       
       sellpoint: "You will feel and will be much healthier if you stick to eating nutritious food. So if you chang up your diet for important health reasons, why won't you switch to clean beauty products for similar reasons?",
 
-      faceserum: "My all-natural, organic Facial Serum is an anti-aging and anti-acne face treatment designed to hydrate and replenish the skin. Nutrients in the serum support cell regeneration and prevent cell damage. It is rich in antioxidants and anti-inflammatory properties that help deeply moisturize, reduces and prevents redness, acne scars, fine lines and wrinkles.",
+      faceserum: "Our Facial Serum is an anti-aging and anti-acne face treatment designed to hydrate and replenish the skin. Tired of red scars? The essential nutrients in this serum support cell regeneration and prevent cell damage! It is rich in antioxidants and anti-inflammatory properties that help deeply moisturize, help clear acne scars, fine lines and wrinkles, leaving you with beautiful healthy looking skin.",
 
       fsingredients: "Organic Roseship Seed Oil, Organin Hemp Seed Oil, Organic Pomengranate Seed Oil, Organic Grape Seed Oil, Organic Moroccan Argan Oil",
 
-      hserum: "My all-natural, organic Hair Serum is a daily hair treatment designed to vitalize and adds lots of shine yo your hair, leaving it looking beautifully conditioned! It is packed with pure, hydrating ingredients that promote healthy hair by strengthening and moisturizing. It helps prevent and control frizziness, humidity, and adds shine without making your hair look greasy! It also promotes hair growth and eliminates dandruff when massaged to the scalp twice a week. Essential oils blended in the serum are known to refresh, energize, and uplift your mood!",
+      hairserum: "Our Organic Hair Serum is a daily hair treatment designed to vitalize and add lots of shine to your hair, leaving it looking beautifully conditioned regularly! Packed with  hydrating ingredients, this product is destined to produce healthy hair while controling frizziness, leaving you with a beautiful and non-greasy look! Essential oils blended in the serum are also known to refresh, energize, and uplift your mood!",
 
       hsingredients: "Organin Jojoba Oil, Organic Grape Seed Oil, Organic Moroccan Argan Oil, Bergamot, Grapefruit, Orange, Lime, and Lemon Organin essential Oils."
       
