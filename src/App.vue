23e704b9a827ba579cbd88ac1c77d6718f95edd7
Andy Yglecias <andyyglecias@gmail.com>
2020-07-20 15:16:32 -0500

    removed address from footer

--- src/App.vue
+++ src/App.vue
@@ -48,13 +48,7 @@
                     <div class="row">
                         <div class="col-md-3">
                             <h2 class="script">Glowing Garden</h2>
-                            <address>
-                                <strong>Twitter, Inc.</strong>
-                                <br>795 Folsom Ave, Suite 600
-                                <br>San Francisco, CA 94107
-                                <br>
-                                <abbr title="Phone">P:</abbr> (123) 456-7890
-                            </address>
+                            
                         </div>
                         <div class="col-md-3">
                             <h4>Handmade</h4>
